1a5bb33e1c57e9708d9f4735d18c8ca5fbab4bf9
vChavezB <47216966+vChavezB@users.noreply.github.com>
2022-01-15 11:56:15 +0100

    Update qs_bsp.cpp
    changed according to qp arduino repo

--- libraries/qpcpp_arm-cm/src/qs_bsp.cpp
+++ libraries/qpcpp_arm-cm/src/qs_bsp.cpp
@@ -16,7 +16,14 @@ QP::QSTimeCtr QP::QS::onGetTime(void)
 
 void QP::QS::onFlush(void)
 {
-    Serial.flush();
+    uint16_t len = 0xFFFFU; // big number to get as many bytes as available
+    uint8_t const *buf = QS::getBlock(&len); // get continguous block of data
+    while (buf != nullptr) { // data available?
+        Serial.write(buf, len); // might poll until all bytes fit
+        len = 0xFFFFU; // big number to get as many bytes as available
+        buf = QS::getBlock(&len); // try to get more data
+    }
+    Serial.flush(); // wait for the transmission of outgoing data to complete
 }
 
 void QP::QS::onReset(void)
